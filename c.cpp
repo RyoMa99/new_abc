7d64c2966cf84294888a0f6cdc3280a55b88ddcb
rym49 <hatanakaryoma.130@gmail.com>
2022-01-26 06:59:50 +0900

    126

---
+++ c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define ce(s) cout<< s << endl;
+#define sp(i) cout<< fixed << setprecision((int)i); // 浮動小数点の表示桁数を指定
+#define rep(ri,rj,rk) for (int ri = (int)rj; ri < (int)rk; ri++)
+#define rrep(ri,rj,rk) for (int ri = (int)rj; ri > (int)rk; ri--)
+#define all(x) x.begin(), x.end()
+
+int main()
+{
+  int n,k; cin >> n >> k;
+  double ans = 0;
+  rep(i,1,n+1)
+  {
+    double tmp = 1.0/n;
+    int point = i;
+    while(point < k)
+    {
+      tmp /= 2.0;
+      point *= 2;
+    }
+    ans += tmp;
+  }
+
+  sp(10);
+  ce(ans);
+  return 0;
+}
