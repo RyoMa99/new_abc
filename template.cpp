d22b7c89a26d6c416e26ed89caea5178b0019837
rym49 <hatanakaryoma.130@gmail.com>
2022-02-07 23:37:50 +0900

    llのfor分を追加

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@ typedef long long ll;
 #define ce(s) cout<< s << endl;
 #define sp(i) cout<< fixed << setprecision((int)i); // 浮動小数点の表示桁数を指定
 #define rep(ri,rj,rk) for (int ri = (int)rj; ri < (int)rk; ri++)
+#define lrep(ri,rj,rk) for (ll ri = (ll)rj; ri < (ll)rk; ri++)
 #define rrep(ri,rj,rk) for (int ri = (int)rj; ri > (int)rk; ri--)
 #define all(x) x.begin(), x.end()
 // 最大公約数 ユークリッドの互除法
