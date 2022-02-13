ef4b987cef992bea99d148b2d432710b9424ab61
rym49 <hatanakaryoma.130@gmail.com>
2022-02-13 22:34:31 +0900

    215

---
+++ c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define ce(s) cout<< s << endl;
+#define sp(i) cout<< fixed << setprecision((int)i); // 浮動小数点の表示桁数を指定
+#define rep(ri,rj,rk) for (int ri = (int)rj; ri < (int)rk; ri++)
+#define lrep(ri,rj,rk) for (ll ri = (ll)rj; ri < (ll)rk; ri++)
+#define rrep(ri,rj,rk) for (int ri = (int)rj; ri > (int)rk; ri--)
+#define all(x) x.begin(), x.end()
+// 最大公約数 ユークリッドの互除法
+template <typename T>
+T gcd(T a, T b)
+{
+  if (a%b == 0) return b;
+  else return gcd(b,a%b);
+}
+
+// 最大公倍数
+template <typename T>
+T lcm(T a, T b)
+{
+  return a*b / gcd(a,b);
+}
+
+
+int main()
+{
+  string s; cin >> s;
+  int k; cin >> k;
+
+  vector<char> vec(all(s));
+  sort(all(vec));
+
+  vector<string> ans;
+  do {
+    string str(all(vec));
+    ans.push_back(str);
+  }while(next_permutation(all(vec)));
+
+  ce(ans[k-1]);
+  return 0;
+}
